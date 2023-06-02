c46e4b3c6d956273e7821d5f4db818d4eb3cf63d
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2023-06-02 09:09:58 -0400

    zz

--- osu_dreamer_kerass/model/beta_schedule.py
+++ osu_dreamer_kerass/model/beta_schedule.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 
 import torch
 import torch.nn.functional as F
+import math
 
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -9,9 +10,18 @@ from tensorflow.keras import layers
 from osu_dreamer.signal import X_DIM
 
 def extract(a, ts, x_shape):
-    batch_size = tf.shape(a)[0]
+    batch_size = a.shape[0]
     out = tf.gather(a, tf.cast(ts, tf.int32), axis=-1)
     return tf.reshape(out, (batch_size, *((1,) * (len(x_shape) - 1))))
+
+def no_gradients(func):
+    @tf.custom_gradient
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        def grad(*grad_outputs):
+            return [None for _ in args]
+        return result, grad
+    return wrapper
 
 class BetaSchedule:
     def __init__(self, betas, net):
@@ -41,58 +51,16 @@ class BetaSchedule:
     def q_sample(self, x, ts, noise=None):
         """sample q(x_t|x_0) using the nice property"""
         if noise is None:
-            noise = tf.random.uniform(tf.shape(x))
+            noise = tf.random.uniform(tf.shape(x)) # was the random.normal? i forgot
         
-        sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, ts, tf.shape(x))
-        sqrt_one_minus_alphas_cumprod_t = extract(self.sqrt_one_minus_alphas_cumprod, ts, tf.shape(x))
-
-        return sqrt_alphas_cumprod_t * x + sqrt_one_minus_alphas_cumprod_t * noise
-
-        # All code below here is not part of the keras implementation
-
-
-class BetaSchedule:
-    def __init__(self, betas, net):
-        self._net = net
-        
-        # define beta schedule
-        self.betas = betas
-        self.timesteps = len(betas)
-
-        # define alphas 
-        self.alphas = 1. - self.betas
-        self.alphas_cumprod = torch.cumprod(self.alphas, axis=0)
-        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0)
-        self.sqrt_recip_alphas_cumprod = torch.rsqrt(self.alphas_cumprod)
-        self.sqrt_recipm1_alphas_cumprod = torch.sqrt(1.0/self.alphas_cumprod - 1)
-
-        # calculations for diffusion q(x_t | x_{t-1}) and others
-        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
-        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod)
-
-        # calculations for posterior q(x_{t-1} | x_t, x_0)
-        self.posterior_mean_x0_coef = self.betas * torch.sqrt(self.alphas_cumprod_prev) / (1. - self.alphas_cumprod)
-        self.posterior_mean_xt_coef = (1.0 - self.alphas_cumprod_prev) * torch.sqrt(self.alphas) / (1.0 - self.alphas_cumprod)
-        self.posterior_variance = self.betas * (1. - self.alphas_cumprod_prev) / (1. - self.alphas_cumprod) # beta_tilde
-        assert (self.posterior_variance[1:] != 0).all(), self.posterior_variance[1:]
-        
-    def net(self, x,a,ts):
-        """predict the mean of the noise added to `x` at timestep `ts`"""
-        return self._net(x,a,ts)
-
-    def q_sample(self, x: "N,X,L", ts: "N,", noise=None) -> "N,X,L":
-        """sample q(x_t|x_0) using the nice property"""
-        if noise is None:
-            noise = torch.randn_like(x)
-
         sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, ts, x.shape)
         sqrt_one_minus_alphas_cumprod_t = extract(self.sqrt_one_minus_alphas_cumprod, ts, x.shape)
 
         return sqrt_alphas_cumprod_t * x + sqrt_one_minus_alphas_cumprod_t * noise
-
+    
     def q_posterior_mean_var(self, x0, x, ts):
         """sample from q(x_{t-1} | x_t, x_0)"""
-
+        
         posterior_mean_x0_coef_t = extract(self.posterior_mean_x0_coef, ts, x.shape)
         posterior_mean_xt_coef_t = extract(self.posterior_mean_xt_coef, ts, x.shape)
 
@@ -103,53 +71,54 @@ class BetaSchedule:
     
     def p_eps_mean_var(self, x, a, ts):
         """sample from p(x_{t-1} | x_t)"""
-        model_eps = self.net(x,a,ts)
-        
+        model_eps = self.net(x, a, ts)
+
         sqrt_recip_alphas_cumprod_t = extract(self.sqrt_recip_alphas_cumprod, ts, x.shape)
         sqrt_recipm1_alphas_cumprod_t = extract(self.sqrt_recipm1_alphas_cumprod, ts, x.shape)
 
-        model_x0 = sqrt_recip_alphas_cumprod_t * x - model_eps * sqrt_recipm1_alphas_cumprod_t # nice property
+        model_x0 = sqrt_recip_alphas_cumprod_t * x - model_eps * sqrt_recipm1_alphas_cumprod_t
         model_mean, model_var = self.q_posterior_mean_var(model_x0, x, ts)
-        
-        return model_eps, model_mean, model_var, model_x0
-        
-    @torch.no_grad()
-    def sample(self, a: "N,A,L", x: "N,X,L" = None) -> "N,X,L":
-        """sample p(x)"""
-        
-        b,_,l = a.size()
-        
-        if x is None:
-            # start from pure noise (for each example in the batch)
-            x = torch.randn((b,X_DIM,l), device=a.device)
 
-        print()
-        for i in tqdm(list(reversed(range(self.timesteps))), desc='sampling loop time step'):
-            ts = torch.full((b,), i, device=a.device, dtype=torch.long)
-            
-            _, model_mean, model_var, _ = self.p_eps_mean_var(x,a,ts)
+        return model_eps, model_mean, model_var, model_x0
+
+    @no_gradients()
+    def sample(self, a, x = None):
+        """sample p(x)"""
+
+        b, _, l = a.shape
+
+        if x is None:
+            # start from pure noise
+            x = tf.random.normal((b, X_DIM, l), dtype=a.dtype)
+        
+        print() # The orginal code has an empty print here for some reason
+        for i in tqdm(list(reversed(range(self.timestamps))), desc="Sampling loop time step"):
+            ts = tf.fill((b,), i, dtype=tf.int64)
+
+            _, model_mean, model_var, _ = self.p_eps_mean_var(x, a, ts)
 
             if i == 0:
                 x = model_mean
             else:
-                x = model_mean + torch.sqrt(model_var) * torch.randn_like(x)
+                x = model_mean + tf.sqrt(model_var) * tf.random.normal(tf.shape(x), dtype=x.dtype)
             
         return x
-    
+
+    # All code below here is not part of the keras implementation
 class CosineBetaSchedule(BetaSchedule):
     def __init__(self, timesteps, net, s=0.008):
         """
         cosine schedule as proposed in https://arxiv.org/abs/2102.09672
         """
         steps = timesteps + 1
-        x = torch.linspace(0, timesteps, steps)
-        alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+        x = tf.linspace(0, timesteps, steps)
+
+        alphas_cumprod = tf.cos(((x / timesteps) + s) / (1 + s) * tf.constant(math.pi) * 0.5) ** 2
         alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
         betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
-        betas = torch.clip(betas, 0.0001, 0.9999)
+        betas = tf.clip(betas, 0.0001, 0.9999)
 
         super().__init__(betas, net)
-
     
 class StridedBetaSchedule(BetaSchedule):
     def __init__(self, schedule, steps, ddim, *args, **kwargs):
